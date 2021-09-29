b1fc0b358b99007fd6a104982f4ff8e22ea610ad
AmeliaAdhariani <ameliaadhariani42066@mhs.unida.ac.id>
2021-09-29 21:53:26 +0700

    menambahakan 1 database di pertemuan4

---
+++ Kuliah/pertemuan4/latihan1.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=  , initial-scale=1.0">
+  <title>Daftar Mahasiswa</title>
+</head>
+<body>
+  <h3>Daftar Mahasiswa</h3>
+
+  <table border="1" cellpadding="10" cellspacing="0 ">
+    <tr>
+      <th>#</th>
+      <th>id_mhs</th>
+      <th>Nama</th>
+      <th>NIM</th>
+      <th>Konsulat</th> 
+      <th>Asrama</th> 
+      <th>Aksi</th> 
+    </tr>
+    <tr>
+      <td>1</td>
+      <td>01</td>
+      <td>Amelia Adhariani</td>
+      <td>422021618066</td>
+      <td>jawa Barat</td>
+      <td>Siti Aminah 115</td>
+      <td>
+        <a href="">ubah</a> | <a href="">hapus</a>
+      </td>
+    </tr>
+  </table>
+
+</body>
+
+
+
+
+    
+
+    
+
+  
+</body>
+</html>
